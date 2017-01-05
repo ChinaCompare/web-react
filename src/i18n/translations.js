fd305e470751846d4cda763742d0b21f7badf419
d0whc3r <d0whc3r@gmail.com>
2017-01-05 01:52:12 +0100

    translations before inject

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -2,17 +2,10 @@
 var merge = require('deepmerge');
 
 // translate:start
-const transca = require('../components/App/translate/locale-ca').translate;
-const transen = require('../components/App/translate/locale-en').translate;
-const transes = require('../components/App/translate/locale-es').translate;
-const trans = {
-  ca: transca,
-  en: transen,
-  es: transes
-};
 // translate:end
 
 // eslint-disable-next-line
 export const translate = trans;
 
+// eslint-disable-next-line
 export default trans;
